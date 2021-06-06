7abd1fe4bbec78e99a09556de2b2fe65e8b47939
Liam Blake <liamlemblake@gmail.com>
2021-06-06 19:01:06 +0930

    more cd aliases

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,7 +1,3 @@
-# Allow easy access of dotfiles path
-export DOTFILES_DIR
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 # System symlinks
 ln -si $(pwd)/system/.bash_aliases "$HOME"
 
