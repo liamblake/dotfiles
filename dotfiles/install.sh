f30f212c2bc1fe79beec6784899839ae05d57c78
Liam Blake <liamlemblake@gmail.com>
2021-06-12 19:00:54 +0930

    Symlink tex style files

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -6,3 +6,8 @@ ln -si $(pwd)/system/.bash_aliases "$HOME"
 
 # git symlinks
 ln -si $(pwd)/git/.gitconfig "$HOME"
+
+# TeX style files
+mkdir -p "$HOME/texmf/tex/latex/"
+ln -si $(pwd)/../tex/latex/* "$HOME/texmf/tex/latex/"
+# TODO: Shared/local
