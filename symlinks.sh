0a02fcd459c0644ba68669cdd3db0cb0a7ee6a27
LiamBlake <liamlemblake@gmail.com>
2021-07-22 22:02:01 +0930

    tmux config

--- symlinks.sh
+++ symlinks.sh
@@ -1,12 +1,12 @@
+# Make .config dir if it doesn't already exist
+mkdir -p "$HOME/.config"
+
 # Create links in .config/dir
 for dir in "git" "terminator"; do
     ln -si $(pwd)/config/$dir "$HOME"/.config/
 done
 
 ### The following symlinks behave differently ###
-# Git config
-ln -si $(pwd)/git/.gitconfig "$HOME"/
-
 # Bash configs
 for dir in ".bash_aliases" ".bashrc"; do
     ln -si $(pwd)/bash/$dir "$HOME"/
@@ -29,3 +29,18 @@ ln -si $(pwd)/config/.jupyter/jupyter_notebook_config.py "$HOME/.jupyter/"
 
 # Install .fzf
 source $(pwd)/ext/fzf/install --completion --key-bindings --all
+
+# tmux setup
+# TPM setup
+if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
+ln -si $(pwd)/tmux/tmux.conf "$HOME"/.tmux.conf
+
+# Taken from https://github.com/samoshkin/tmux-config/blob/master/install.sh
+tmux new -d -s __noop >/dev/null 2>&1 || true 
+tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
+"$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
+tmux kill-session -t __noop >/dev/null 2>&1 || true
