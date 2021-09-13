49f9cce08371c91af5caa4e55c933c8d1b399763
LiamBlake <liamlemblake@gmail.com>
2021-09-13 15:34:17 +0930

    Daily tweaks

--- config/nvim/lua/colours.lua
+++ config/nvim/lua/colours.lua
@@ -5,6 +5,7 @@ vim.opt.termguicolors = true
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_keywords = false
 vim.g.tokyonight_lualine_bold = true
+vim.g.tokyonight_brightness = 0.1
 vim.cmd("colorscheme tokyonight")
 
 -- This gets the colours defined by tokyonight
--- config/nvim/pythonx/snippet_helpers.py
+++ config/nvim/pythonx/snippet_helpers.py
@@ -5,6 +5,8 @@ https://github.com/honza/vim-snippets
 
 import re
 
+import vim
+
 
 def complete(tab, opts):
     """
@@ -25,3 +27,23 @@ def complete(tab, opts):
     if len(opts) > 5:
         cads += "|..."
     return "({0})".format(cads)
+
+
+# Tests for the existence of a variable declared by Vim's filetype detection
+# suggesting the type of shell script of the current file
+def test_shell(scope, shell):
+    return vim.eval("exists('" + scope + ":is_" + shell + "')")
+
+
+# Loops over the possible variables, checking for global variables
+# first since they indicate an override by the user.
+def get_shell():
+    for scope in ["g", "b"]:
+        for shell in ["bash", "sh", "zsh"]:
+            if test_shell(scope, shell) == "1":
+                if shell == "kornshell":
+                    return "ksh"
+                if shell == "posix":
+                    return "sh"
+                return shell
+    return "sh"
