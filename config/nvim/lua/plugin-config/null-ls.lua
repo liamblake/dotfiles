1d2a95f443c5f528813dbcee4020372f36baf654
LiamBlake <liamlemblake@gmail.com>
2021-09-19 16:08:11 +0930

    Apply stylua to null-ls.lua

--- config/nvim/lua/plugin-config/null-ls.lua
+++ config/nvim/lua/plugin-config/null-ls.lua
@@ -3,21 +3,20 @@ local M = {}
 local formatting = require("null-ls").builtins.formatting
 local linting = require("null-ls").builtins.diagnostics
 
-
 M.setup = function()
 	local null_ls = require("null-ls")
 	local nl_utils = require("null-ls.helpers")
 
-local indentlatex = {
-	method = require("null-ls.methods").FORMATTING,
-	filetypes = { "latex" },
-	generator = {},
-	generator_opts = {
-		command = "latexindent",
-		to_stdin = true,
-	},
-	factory = nl_utils.formatter_factory,
-}
+	local indentlatex = {
+		method = require("null-ls.methods").FORMATTING,
+		filetypes = { "latex" },
+		generator = {},
+		generator_opts = {
+			command = "latexindent",
+			to_stdin = true,
+		},
+		factory = nl_utils.formatter_factory,
+	}
 
 	require("null-ls").config({
 		sources = {
