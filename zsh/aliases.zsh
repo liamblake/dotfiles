9e919807112f2d289909aeff447b14b8d1e6fe60
LiamBlake <liamlemblake@gmail.com>
2021-12-03 11:16:22 +1030

    [zsh]: Remove unused aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,18 +3,12 @@ alias la="ls -a"
 # Commonly used directories - assumes dev dir is in home
 alias dev="cd $HOME/dev"
 alias dbx="cd $HOME/Dropbox"
-alias uni="cd $HOME/Dropbox/university"
-
-alias mail="thunderbird"
-
-# Quickly open and edit dotfiles from anywhere
-alias editdot="code -n $HOME/dev/dotfiles"
 
 alias tmux='tmux -2'
-alias mux=tmuxinator
 
 # Commonly used commands
 alias g="git"
+alias mux="tmuxinator"
 
 # Neovim with startup time logged
 alias nvimst="nvim --startuptime startup.log"
