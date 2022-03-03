6d80622b97e59532d0852117d63f1d3f6c4c1c84
LiamBlake <liamlemblake@gmail.com>
2022-03-03 16:14:55 +1030

    [julia]: Using Revise on startup

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -1,6 +1,12 @@
 # Load some useful packages
 using Pkg
 
+try
+	using Revise
+catch e
+	@warn "Error initialising Revise" exception = (e, catch_backtrace())
+end
+
 # If Julia is started in a directory
 # TODO: Add a custom flag to overwrite this
 if isfile("Project.toml") && isfile("Manifest.toml")
