eb35e4b5c30519b6dce58638ecb8c726401756e8
liamblake <liamlemblake@gmail.com>
2024-07-24 11:15:44 +0930

    Simplify symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -1,36 +1,15 @@
-# Absolute path of root of dotfiles
-DOTFILES_ROOT=$(cd "$(dirname "$0")"; pwd -P)
-
-Echo $SCRIPT
-echo $DOTFILES_ROOT
-
 create_home_symlink() {
-	SRC=$DOTFILES_ROOT/$1
+	SRC=$(pwd)/$1
 	TARGET=$HOME/$2
 
 	ln -si $SRC $TARGET
 }
 
-# Create links in home
-for dir in "zsh" "formatting" "tmux"; do
-	for f in $(ls -a $DOTFILES_ROOT/$dir); do
-		create_home_symlink $dir/$f .
-	done
-done
-
-# These files are placed in other hidden directories in home
-for dir in "julia"; do
-	for f in $(ls -a $DOTFILES_ROOT/$dir); do
-		create_home_symlink $dir/$f .$dir/
-	done
-done
-
 # Make .config dir if it doesn't already exist
 mkdir -p "$HOME/.config"
 
 # Create links in ~/.config/dir
-# TODO: Move more files to .config, will reduce repetition in this file.
-for dir in "git" "tmuxinator" "nvim" "alacritty" "rstudio"; do
+for dir in "git" "tmuxinator" "nvim" "alacritty" "rstudio" "zsh" "tmux"; do
 	create_home_symlink config/$dir .config/
 done
 
@@ -39,9 +18,6 @@ mkdir -p "$HOME"/.config/Code/User/
 create_home_symlink config/vscode/settings.json .config/Code/User
 
 # TODO: TeX style files
-mkdir -p "$HOME"/texmf
-create_home_symlink tex texmf/
-# create_home_symlink tex .miktex/texmfs/install/lb/te
 
 # TPM setup
 if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
@@ -53,8 +29,3 @@ tmux new -d -s __noop >/dev/null 2>&1 || true
 tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
 "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
-
-# If dev isn't already in home, add a symlink
-if [ ! -d "$HOME"/dev ]; then
-	create_home_symlink /../../dev/ .
-fi
