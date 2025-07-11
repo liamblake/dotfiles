3ade93e1b3a092c67b2b1c62a26a930db0db9387
liamblake <liamlemblake@gmail.com>
2025-07-11 14:24:38 +0930

    [julia] template_academic in startup file

--- julia/config/startup.jl
+++ julia/config/startup.jl
@@ -2,9 +2,9 @@
 using Pkg
 
 try
-	using Revise
+    using Revise
 catch e
-	@warn "Error initialising Revise" exception = (e, catch_backtrace())
+    @warn "Error initialising Revise" exception=(e, catch_backtrace())
 end
 
 # If Julia is started in a directory
@@ -14,25 +14,31 @@ end
 
 # Include the current project in the prompt
 try
-	import OhMyREPL as OMR
+    import OhMyREPL as OMR
 catch e
-	@warn "Error initialising OhMyREPL" exception = (e, catch_backtrace())
+    @warn "Error initialising OhMyREPL" exception=(e, catch_backtrace())
 else
-	promptfn() = "(" * splitpath(Base.active_project())[end-1] * ") julia> "
-	OMR.input_prompt!(promptfn)
+    promptfn() = "(" * splitpath(Base.active_project())[end - 1] * ") julia> "
+    OMR.input_prompt!(promptfn)
 
-	# No syntax highlighting
-	OMR.enable_pass!("SyntaxHighlighter", false)
-	OMR.enable_pass!("RainbowBrackets", false)
+    # No syntax highlighting
+    OMR.enable_pass!("SyntaxHighlighter", false)
+    OMR.enable_pass!("RainbowBrackets", false)
 
-	ENV["JULIA_EDITOR"] = "nvim"
+    ENV["JULIA_EDITOR"] = "nvim"
 end
 
-# PkgTemplate 
-function template()
-	@eval begin 
-		using PkgTemplates
-		Template(;)
-	end
+# PkgTemplates
+function template_academic()
+    @eval begin
+        using PkgTemplates
+        Template(; user = "liamblake",
+            authors = "Liam A. A. Blake <liam.blake@adelaide.edu.au>",
+            dir = ".",
+            interactive = true,
+            plugins = [
+                ProjectFile(), SrcDir(), Tests(), Readme(), License(), Git(), GithubActions(),
+                CompatHelper(), TagBot(), Secret(), Dependabot(), Documenter(),
+                Formatter(; file = "~/dev/dotfiles/formatting/.JuliaFormatter.toml")])
+    end
 end
-
